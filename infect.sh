cdf48a933ba34560f9b78ed4170838000e79034f
HankelBao <hankelbao@outlook.com>
2017-06-16 14:50:19 +0800

    OK now?

--- infect.sh
+++ infect.sh
@@ -1,6 +1,9 @@
 ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
 ln -s ~/dotfiles/vim/.vim ~/.vim
 ln -s ~/dotfiles/vim/.vim-config/ ~/.vim-config
+cd ~/autoload/
+git clone https://github.com/junegunn/vim-plug.git
+
 #ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 #ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
 #ln -s ~/dotfiles/zsh/.zsh_history ~/.zsh_history
---
+++ vim/.vim/.vim
@@ -0,0 +1,1 @@
+/home/hankelbao/dotfiles/vim/.vim
