55caf7f10d41c6d388de1733668cc7761e13318c
HankelBao <hankelbao@outlook.com>
2017-06-16 16:31:30 +0800

    Divide build and infect

--- infect.sh
+++ infect.sh
@@ -1,9 +1,2 @@
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
 ln -s ~/dotfiles/vim/.vim-config/ ~/.vim-config
-
-#ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
-#ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
-#ln -s ~/dotfiles/zsh/.zsh_history ~/.zsh_history
-#ln -s ~/dotfiles/zsh/.oh-my-zsh ~/.oh-my-zsh
