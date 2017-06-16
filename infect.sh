b33692e6ec054ba8642b421182af1ba94d0215f9
HankelBao <hankelbao@outlook.com>
2017-06-16 16:20:13 +0800

    now it works

--- infect.sh
+++ infect.sh
@@ -3,10 +3,6 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
 ln -s ~/dotfiles/vim/.vim-config/ ~/.vim-config
 
-mkdir ~/.vim
-mkdir ~/.vim/autoload
-mkdir ~/.vim/plugged
-
 #ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 #ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
 #ln -s ~/dotfiles/zsh/.zsh_history ~/.zsh_history
