d3b4daa13a8b0851fad6db3e3c7057ce976ab20f
HankelBao <hankelbao@outlook.com>
2017-10-26 19:09:18 +0800

    tmux

--- tmux/build.sh
+++ tmux/build.sh
@@ -1,4 +1,4 @@
 rm ~/.tmux.conf
 rm -rf ~/.tmux-themepack
 ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/tmux/.tmux-themepack ~/.tmux-themepack
+ln -s ~/dotfiles/tmux/.basic.tmuxtheme ~/.basic.tmuxtheme
