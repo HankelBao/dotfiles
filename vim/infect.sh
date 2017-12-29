555f4de2b9184bf14b6a737cb0ca6a173520909c
HankelBao <hankelbao@outlook.com>
2017-12-29 12:35:00 +0800

    add force to soft link

--- vim/infect.sh
+++ vim/infect.sh
@@ -1,4 +1,4 @@
 rm -rf ~/.vim-config
 rm ~/.vimrc
-ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
-ln -s ~/dotfiles/vim/.vim-config/ ~/.vim-config
+ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/vim/.vim-config/ ~/.vim-config
